d00a2fe4696d6835c7e4fec01cc21c5184a4c21c
ahmaddorra <ahmadeldorra1@gmail.com>
2022-06-29 15:43:54 +0300

    .

--- app.py
+++ app.py
@@ -28,5 +28,6 @@ def predict():
     return jsonify({"category": str(prediction)})
     
   
-if __name__ == '__main__':
-  app.run(host='0.0.0.0',debug=True, port=os.environ.get("PORT", 5000))
+# Create the main driver function
+port = int(os.environ.get("PORT", 5000)) # <-----
+app.run(host='0.0.0.0', port=port)       # <-----
